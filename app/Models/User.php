5e93f900cb382341b210422884121e5a0362a5a2
Chen Yi-Hsuan <s3a932087@student.ncut.edu.tw>
2023-01-03 22:32:37 +0800

    建立user與chirps的關聯

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,10 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function chirps(){
+        return $this->hasMany(Chirp::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
